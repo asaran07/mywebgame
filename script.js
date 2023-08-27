195986f3650644f361426f456991087db46af916
asaran07 <saran.arsh07@icloud.com>
2023-08-26 18:15:53 -0700

    character movement

--- script.js
+++ script.js
@@ -1,25 +1,88 @@
 const canvas = document.getElementById("gameArea");
 const ctx = canvas.getContext("2d");
 
-let x = 100;
-let y = 100;
-let radius = 50;
+class Player {
+    constructor() {
+        this.x = 100;
+        this.y = 100;
+        this.width = 20;
+        this.height = 60;
+        this.color = "yellow";
+        this.speed = 5;
+    }
 
-function drawGame() {
-    refreshCanvas();
-    refreshPlayer();
+    inputs() {
+        if (downPressed) {
+            this.y += this.speed;
+        }
+        if (upPressed) {
+            this.y -= this.speed;
+        }
+        if (leftPressed) {
+            this.x -= this.speed;
+        }
+        if (rightPressed) {
+            this.x += this.speed;
+        }
+    }
+
+    draw() {
+        ctx.fillStyle = player.color;
+        ctx.beginPath();
+        ctx.fillRect(player.x, player.y, player.width, player.height);
+    }
 }
 
-function refreshPlayer() {
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.arc(x,y, radius,0, Math.PI * 2);
-    ctx.fill();
+const player = new Player();
+
+let downPressed = false;
+let upPressed = false;
+let leftPressed = false;
+let rightPressed = false;
+
+document.body.addEventListener("keydown", keyDown);
+document.body.addEventListener("keyup", keyUp)
+
+function drawGame() {
+    requestAnimationFrame(drawGame);
+    refreshCanvas();
+    player.inputs();
+    player.draw();
 }
 
 function refreshCanvas() {
     ctx.fillStyle = "black";
     ctx.fillRect(0,0, canvas.clientWidth, canvas.clientHeight)
+}
+
+function keyDown(event) {
+    if(event.keyCode == 40) {
+        downPressed = true;
+    }
+    if (event.keyCode == 38) {
+        upPressed = true;
+    }
+    if (event.keyCode == 37) {
+        leftPressed = true;
+    }
+    if (event.keyCode == 39) {
+        rightPressed = true;
+    }
+}
+
+function keyUp(event) {
+    if (event.keyCode == 40) {
+        downPressed = false;
+    }
+    if (event.keyCode == 38) {
+        upPressed = false;
+    }
+    if (event.keyCode == 37) {
+        leftPressed = false;
+    }
+    if (event.keyCode == 39) {
+        rightPressed = false;
+    }
 }
 
 drawGame();
